c5f5f02243697d51b7c5ebb48cc28fe390bc31f1
Qiuyang Mang <qmang@berkeley.edu>
2025-12-16 03:14:22 +0800

    fix chk.cpp for #43

--- algorithmic/problems/43/chk.cpp
+++ algorithmic/problems/43/chk.cpp
@@ -209,9 +209,7 @@ int main(int argc, char* argv[]) {
         quitf(_wa, "The puzzle is unsolvable");
     }
 
-    // Scoring:
-    // - score_ratio: clamped to [0, 1], baseline 63,000 moves  -> 1.0
-    // - unbounded_ratio: clamped to [0, 1], baseline 100,000 moves -> 1.0
+
     long long score = bestMoves;
     double score_ratio = bestMoves <= 0 ? 0.0 : (double)bestMoves / 63000.0;
     double unbounded_ratio = score_ratio;
